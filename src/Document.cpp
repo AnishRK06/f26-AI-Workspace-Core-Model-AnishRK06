24b6675de30709110238aef0d14e54115a6f6739
AnishRK06 <kankanalaranish@gmail.com>
2026-09-03 17:02:34 -0400

    src files done

--- src/Document.cpp
+++ src/Document.cpp
@@ -1,51 +1,79 @@
 #include "Document.hpp"
 
-Document::Document(std::string title, std::string contents) {
-    // TODO: implement according to the M1 specification.
+#include <fstream>
+#include <ios>
+#include <iterator>
+#include <utility>
 
+Document::Document(std::string title, std::string contents) {
+    // sourcePath_ stays empty, this one did not come from a file
+    title_ = std::move(title);
+    contents_ = std::move(contents);
 }
 
 bool Document::operator==(const Document& other) const {
-    // TODO
-
+    return title_ == other.title_ &&
+           sourcePath_ == other.sourcePath_ &&
+           contents_ == other.contents_;
 }
 
 bool Document::operator!=(const Document& other) const {
-    // TODO
-
+    return !(*this == other);
 }
 
 bool Document::load(const std::string& path) {
-    // TODO
+    std::ifstream in(path, std::ios::binary);
+    if (!in) {
+        return false;
+    }
 
+    // read into a local, only commit if it worked
+    std::string data;
+    try {
+        data.assign(std::istreambuf_iterator<char>(in),
+                    std::istreambuf_iterator<char>());
+    } catch (const std::ios_base::failure&) {
+        // opening a directory succeeds but reading it throws
+        return false;
+    }
+    if (in.bad()) {
+        return false;
+    }
+
+    // title is the last path component, "text/sample.txt" -> "sample.txt"
+    std::string::size_type slash = path.find_last_of("/\\");
+    if (slash == std::string::npos) {
+        title_ = path;
+    } else {
+        title_ = path.substr(slash + 1);
+    }
+
+    contents_ = std::move(data);
+    sourcePath_ = path;
+    return true;
 }
 
 const std::string& Document::title() const noexcept {
-    // TODO
-
+    return title_;
 }
 
 const std::string& Document::sourcePath() const noexcept {
-    // TODO
-
+    return sourcePath_;
 }
 
 const std::string& Document::contents() const noexcept {
-    // TODO
-  
+    return contents_;
 }
 
 void Document::setTitle(std::string title) {
-    // TODO
-  
+    title_ = std::move(title);
 }
 
 std::size_t Document::characterCount() const noexcept {
-    // TODO
-    
+    return contents_.size();
 }
 
 bool Document::empty() const noexcept {
-    // TODO
-   
+    // emptiness is about contents, not the title
+    return contents_.empty();
 }
